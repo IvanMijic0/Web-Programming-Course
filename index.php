1057b75fb9e1648c1b3338b530c426859c655da4
IvanMijic0 <ivanmijic159951@gmail.com>
2023-03-01 14:18:57 +0100

    Update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  require "vendor/autoload.php";
 
  Flight::route("/index.php", function(){
-    echo "Pepi, Gabi i Gugi su slatki";
+    echo "Pepi, Gabi i Gugica su slatki";
  });
 
  Flight::start();
